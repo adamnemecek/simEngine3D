fa96e081c59df93857d4a71455f6526bb17e1eff
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-10-13 23:07:02 -0500

    updated DP1 properties, getters and constructor

--- GCons/DP1.m
+++ GCons/DP1.m
@@ -4,25 +4,70 @@ classdef DP1
     %   Detailed explanation goes here
     
     properties
-        Qi; % [3x1] point location of head of vector AiBar
-        Pi; % [3x1] point location of tail of vector AiBar
-        Qj; % [3x1] point location of head of vector AjBar
-        Pj; % [3x1] point location of tail of vector AjBar
-        
+        bodyi; %instance of a body object, i 
+        bodyj; %instance of a body object, j
+        Qi; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        Pi; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        Qj; %index of a [3x1] marker contained within bodyj, representing head of ajBar
+        Pj; %index of a [3x1] marker contained within bodyj, representing head of ajBar
+        f; %prescribed value that the dot product should assume
+        fdot; %derivative of above
+        fddot;%derivative of above       
     end
     
-    %calculated values within
+    %calculated values 
     properties(Dependent)
-        AiBar; % vector in local reference frame of body i
-        AjBar; % vector in local reference frame of body j
+        aiBar; % vector in local reference frame of body i
+        ajBar; % vector in local reference frame of body j
+        phi; %jacobian
+        nu;  % dPhi_dp1/ dt
+        gamma; % RHS of acceleration equation
+        phiR;
+        phiP;
+        
+        
+        
     end
     
     methods
         %constructor
+        function dp1 = DP1(bodyi,bodyj,Qi,Pi,Qj,Pj,f,fdot,fddot)
+            dp1.bodyi = bodyi;
+            dp1.bodyj = bodyj;
+            dp1.Qi = Qi;
+            dp1.Pi = Pi;
+            dp1.Qj = Qj;
+            dp1.Pj = Pj;
+            dp1.f = f;
+            dp1.fdot = fdot;
+            dp1.fddot = fddot;
+            
         
+        %getters for dependent properties
+        function aiBar = get.aiBar() %9.26 slide 10
+            aiBar = dp1.bodyi.markers(Qi)-dp1.bodyi.markers(Pi);
+        end
         
-        %getters fro dependent properties
-        function AiBar = get.A
+        function ajBar = get.ajBar() %9.26 slide 10
+            ajBar = dp1.bodyj.markers(Qj)-dp1.bodyi.markers(Pj);
+        end
+        
+        function phi = get.phi() %9.26 slide 11
+            
+        end
+        
+        function nu = get.nu()
+        end
+        
+        function gamma = get.gamma()
+        end
+       
+        function phiR = phiR()
+        end
+        
+        function phiP = phiP()
+        end
+            
     end
     
 end
