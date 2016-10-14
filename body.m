2443cc3c1caa0a35b64f11b1d24e498105501a4c
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-10-13 22:10:48 -0500

    updated the functions of body class

--- body.m
+++ body.m
@@ -1,4 +1,4 @@
-classdef Body
+classdef Body < handle
     %Body class defines a rigid body and its associated makers
    
     
@@ -15,7 +15,7 @@ classdef Body
         I; %[3x3] inertia tensor of the ridgid body
         
         %markers - used to define constraints
-        markers; % cell array of markers, accessed by their index 
+        markers{1} = [0,0,0]'; % cell array of markers, accessed by their index 
                  % ex) body.markers{1} -> [0,0,0]
                  %first marker should always be the origin
         
@@ -23,10 +23,22 @@ classdef Body
     
     methods
         %default constructor
-        function body = constBody()
+        function body = makeBody(ID,r,p,pdot,m,I,markers)
+            body.ID = ID;
+            body.r = r;
+            body.p = p;
+            body.pdot = pdot;
+            body.m = m;
+            body.I = I;
+            body.markers = markers;
         end
-        
+            
+        %add marker to list of markers
+        function appendMarker(body, newMarker)
+            %input: body object, marker [3x1]
+            body.markers{length(markers) + 1} = newMarker; 
+        end
+            
     end
     
 end
-
