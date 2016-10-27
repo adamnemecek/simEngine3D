7853384755ce4e291fcc2ea8076ac6daeea48499
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-10-27 15:36:56 -0500

    added Euler Angle rotations to MatOp, useful for kinematic mechanism setup

--- MatOp.m
+++ MatOp.m
@@ -8,21 +8,48 @@ classdef MatOp
             %matrix, capable of performing the cross product
             a_tilde = [ 0 -a(3) a(2);
                       a(3)  0  -a(1);
-                     -a(2) a(1)  0];
+                     -a(2) a(1)  0 ];
         end
         
-        %conversion functions from A2P and P2A
+        function A = eulerRot(axis,theta)
+            %perform and euler angle rotation about a specified axis
+            %in the set [x,y,z], by angle theta, CCW
+            %input: 
+                %axis in the set [x,y,z]
+                %theta specified in degrees
+             rad = rad2deg(theta);
+             switch axis
+                case 'x'
+                    A = [1         0        0      ; ...
+                         0       cos(rad) -sin(rad); ...
+                         0       sin(rad)  cos(rad)];
+                case 'y' 
+                    A = [cos(rad)  0       sin(rad); ...
+                         0         1        0      ; ...
+                        -sin(rad)  0       cos(rad)];
+                case 'z'
+                    A = [cos(rad) -sin(rad) 0      ; ...
+                         sin(rad) cos(rad)  0      ; ...
+                         0         0        1      ];
+                otherwise
+                    error('not a principle axis of rotation');
+             end
+        end
+                    
+    
+        
+        %------------conversion functions from A2P and P2A--------------
         function  P = A2P(A)
             %to be implemented
         end
         
-        function A = P2A(P)
+        function A = P2A(p)
             e0 = p(1);
             e = p(2:4);
             %from key kinematic formulas
             E = [-e,  MatOp.tilde(e) +e0*eye(3)];
             G = [-e, -MatOp.tilde(e) +e0*eye(3)];
-            A = EG';
+            A = E*G';
         end
         
         function B = calcB(p,aBar) %9.28 slide 12
@@ -36,6 +63,5 @@ classdef MatOp
         end
         end
         
-    end
-    
+end
 
