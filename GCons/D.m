4f6e62956cfbd88ad6118e76814c1f2e794c7451
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-11-19 22:16:22 -0600

    Finished prototype of DP1 and D constraints

---
+++ GCons/D.m
@@ -0,0 +1,139 @@
+classdef D
+    %D or distance constraint using r-p formulation. The Distance constraint 
+    %exists between a between a vector siBar(defined by Pi) on body i, and
+    %a vector sjBar (defined by point Qj)on body j (see slide 16, lecture
+    %9.26.2016 for a graphical representation). The constraint specifies 
+    %that the distance between the heads of vectors aiBar and Ajbar maintain 
+    %assume a certian value, F(t) which is frequently a constant,
+    %rather than an explicit function of time
+    
+    properties
+        bodyi; %instance of a body object, i 
+        bodyj; %instance of a body object, j
+        %these point definitions are taken from S10 lecture 9.2
+        Pi; %integer index of a [3x1] marker contained within bodyi, representing head of siBar
+        Qj; %integer index of a [3x1] marker contained within bodyj, representing head of sjBar
+        f; %prescribed value that the dot product should assume
+        fdot; %derivative of above
+        fddot;%derivative of above
+        t;
+ 
+    end
+    
+    %calculated values 
+    properties(Dependent)
+        dij %vector in G-RF representing the distance from Pi to Qj
+        dijdot;%derivative of above
+        phi; %jacobian
+        nu;  %  - d/dt(Phi)
+        gamma; %RHS of acceleration equation
+        phi_ri;%derivative of phi wrt ri
+        phi_rj;%derivative of phi wrt rj
+        phi_pi;%derivative of phi wrt pi
+        phi_pj;%derivative of phi wrt pj   
+    end
+    
+    
+    methods
+        %constructor
+        function d = D(bodyi,bodyj,Pi,Qj,f,fdot,fddot,t)
+            d.bodyi = bodyi;
+            d.bodyj = bodyj;
+            d.Pi = Pi;
+            d.Qj = Qj;
+            d.f = f;
+            d.fdot = fdot;
+            d.fddot = fddot;
+            d.t = t;
+            
+        end
+        
+        
+        %-----------getters for dependent properties-----------------------
+        function dij = get.dij(d)
+            %returns the distance vector [3x1] in G-RF that connects point
+            %Pi on body i to point Qj on body j
+            dij = d.bodyj.r + d.bodyj.A*d.bodyj.markers{d.Qj} - ...
+                  (d.bodyi.r - d.bodyi.A*d.bodyi.markers{d.Pi});
+        end
+        
+        function dijdot = get.dijdot(d)
+            %returns the derivative of the distance vector [3x1]
+            dijdot = d.bodyj.rdot + MatOp.B(d.bodyj.p, d.bodyj.marker{Qj})*d.bodyj.pdot ...
+                    -d.bodyi.rdot + MatOp.B(d.bodyi.p, d.bodyi.marker{Pi})*d.bodyi.pdot;
+        end
+        
+        function phi = get.phi(d) %9.26 slide 17
+            %phi is the constraint equation [1x1] which states that 
+            %the magnitude of the distance between Pi and Qj must be 
+            %equal to ft
+            dij = d.dij; %only calculate once
+            phi =  dij'*dij - d.f;
+        end
+        
+        function nu = get.nu(d) %9.26 slide 12
+            nu = d.fdot;
+        end
+        
+        function gamma = get.gamma(d)% 10.7 slide 8
+            %gamma [1x1] is the RHS of the acceleration equation
+            dij = d.dij; %calculate dij once per call
+            dijdot = d.dijdot;
+            bpdoti = MatOp.B(d.bodyi.p,d.bodyi.markers{d.Pi});
+            bpdotj = MatOp.B(d.bodyj.p,d.bodyj.markers{d.Qj});
+            
+            gamma = -2*dij'*bpdotj*d.bodyj.pdot + 2*dij*bpdoti*d.bodyi.pdot + ...
+                    -2*dijdot'*dijdot + d.fddot;   
+        end
+       
+        
+        function phi_ri = get.phi_ri(d) % s16 lecture 9.28.2016
+            %phi_ri [1x3] is the derivative of phi with respect to the 
+            %positional coordinates of bodyi. If I is the ground, then
+            %phi_ri is [0x0]
+            if d.bodyi.isground
+                phi_ri = [];
+            else
+                phi_ri = -2*d.dij';
+            end
+        end
+        
+        function phi_rj = get.phi_rj(d) % s16 lecture 9.28.2016
+            %phi_ri [1x3] is the derivative of phi with respect to the 
+            %positional coordinates of bodyi. If I is the ground, then
+            %phi_rj is [0x0]
+            if d.bodyj.isground
+                phi_rj = [];
+            else
+                phi_rj = 2*d.dij';
+            end
+        end
+       
+        
+        function phi_pi = get.phi_pi(d) % s16 lecture 9.28.2016
+            %phi_pi [1x4] is the derivative of phi with respect to the 
+            %orientational coordinates of bodyi. If bodyi is the ground
+            %then phi_pi is [0x0]
+            if d.bodyi.isground
+                phi_pi = [];
+            else
+                phi_pi = 2*d.dij'*-MatOp.B(d.bodyi.p,d.bodyi.markers{d.Pi});
+            end
+        end
+        
+        function phi_pj = get.phi_pj(d) % s16 lecture 9.28.2016
+            %phi_pi [1x4] is the derivative of phi with respect to the 
+            %orientational coordinates of bodyi. If bodyi is the ground
+            %then phi_pi is [0x0]
+            if d.bodyj.isground
+                phi_pj = [];
+            else
+                phi_pj = 2*d.dij'*MatOp.B(d.bodyj.p,d.bodyj.markers{d.Qj});
+            
+            end
+        end
+           
+            
+    
+    end
+end
--- GCons/DP1.m
+++ GCons/DP1.m
@@ -1,18 +1,22 @@
 classdef DP1
-    %DP1 constraint using r-p. dp acts between two markers, from two 
-    %separate ridgid bodies
-    %   Detailed explanation goes here
+    %DP1 constraint using r-p formulation. The DP1 constraint exists
+    %between a vector aiBar on body i, and a vector ajBar, on body j. DP1
+    %constraint specifies that the dot product of the two vectors aiBar
+    %and ajBar assume a certian value, F(t), which is frequently a constant
     
     properties
         bodyi; %instance of a body object, i 
         bodyj; %instance of a body object, j
+        
+        %these point definitions are taken from S10 lecture 9.26
         Qi; %index of a [3x1] marker contained within bodyi, representing head of aiBar
-        Pi; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        Pi; %index of a [3x1] marker contained within bodyi, representing tail of aiBar
         Qj; %index of a [3x1] marker contained within bodyj, representing head of ajBar
-        Pj; %index of a [3x1] marker contained within bodyj, representing head of ajBar
+        Pj; %index of a [3x1] marker contained within bodyj, representing tail of ajBar
         f; %prescribed value that the dot product should assume
         fdot; %derivative of above
-        fddot;%derivative of above       
+        fddot;%derivative of above 
+        t;  
     end
     
     %calculated values 
@@ -21,9 +25,11 @@ classdef DP1
         ajBar; % vector in local reference frame of body j
         phi; %jacobian
         nu;  % dPhi_dp1/ dt
-        gamma; % RHS of acceleration equation
-        phiR;
-        phiP;
+        gamma; %RHS of acceleration equation
+        phi_ri;%derivative of phi wrt ri
+        phi_rj;%derivative of phi wrt rj
+        phi_pi;%derivative of phi wrt ri
+        phi_pj;%derivative of phi wrt ri
         
         
         
@@ -31,7 +37,7 @@ classdef DP1
     
     methods
         %constructor
-        function dp1 = DP1(bodyi,bodyj,Qi,Pi,Qj,Pj,f,fdot,fddot)
+        function dp1 = DP1(bodyi,bodyj,Qi,Pi,Qj,Pj,f,fdot,fddot,t)
             dp1.bodyi = bodyi;
             dp1.bodyj = bodyj;
             dp1.Qi = Qi;
@@ -41,38 +47,92 @@ classdef DP1
             dp1.f = f;
             dp1.fdot = fdot;
             dp1.fddot = fddot;
+            dp1.t = t;
             
-        
+        end
         %getters for dependent properties
-        function aiBar = get.aiBar() %9.26 slide 10
-            aiBar = dp1.bodyi.markers{Qi}-dp1.bodyi.markers{Pi};
+        
+        function aiBar = get.aiBar(dp1) %9.26 slide 10
+            %ai vector in the local reference frame
+            aiBar = dp1.bodyi.markers{dp1.Qi}-dp1.bodyi.markers{dp1.Pi};
         end
         
-        function ajBar = get.ajBar() %9.26 slide 10
-            ajBar = dp1.bodyj.markers{Qj}-dp1.bodyi.markers{Pj};
+        function ajBar = get.ajBar(dp1) %9.26 slide 10
+            ajBar = dp1.bodyj.markers{dp1.Qj}-dp1.bodyi.markers{dp1.Pj};
         end
         
-        function phi = get.phi() %9.26 slide 11
+        function phi = get.phi(dp1) %9.26 slide 11
             %phi = aiBar'* Ai'* Aj * ajBar
-            phi = (aiBar' * dp1.bodyi.A')*(dp1.bodyj.A * ajBar) - dp1.f;
+            phi = (dp1.aiBar' * dp1.bodyi.A')*(dp1.bodyj.A * dp1.ajBar) - dp1.f;
         end
         
-        function nu = get.nu() %9.26 slide 12
+        function nu = get.nu(dp1) %9.26 slide 12
             nu = dp1.fdot;
         end
         
-        function gamma = get.gamma()% 9.26 slide 12
-            gamma = 
+        function gamma = get.gamma(dp1)% 10.7 slide 8
+            %gamma [1x1] is the RHS of the acceleration equation
+            ai = dp1.bodyi.A*dp1.aiBar; % a = A*abar
+            bpdotj = Matop.B(dp1.bodyj.pdot, dp1.ajBar);
+            pdotj = dp1.bodyj.pdot;
+            aj = dp1.bodyj.A*dp1.ajBar;
+            bpdoti = Matop.B(dp1.bodyi.pdot, dp1.aiBar);
+            pdoti = dp1.bodyi.pdot;
+            adoti = Matop.B(dp1.bodyi.p, dp1.aiBar)*dp1.bodyi.pdot; %aidot = B(p,abar)*pdot
+            adotj = Matop.B(dp1.bodyj.p, dp1.ajBar)*dp1.bodyj.pdot;
+            
+            
+            gamma = -ai'*bpdotj*pdotj - aj'*bpdoti*pdoti - 2*adoti'*adotj + d1.fddot;
             
         end
        
-        function phiR = phiR()
+        function phi_ri = get.phi_ri(dp1) % s13 lecture 9.28.2016
+            %phi_ri [1x3] is the derivative of phi with respect to the 
+            %positional coordinates of bodyi. If I is the ground, then
+            %phi_ri is [0x0]
+            if dp1.bodyi.isground
+                phi_ri = [];
+            else
+                phi_ri = [0 0 0]
+            end
         end
         
-        function phiP = phiP()
+        function phi_rj = get.phi_rj(dp1) % s13 lecture 9.28.2016
+            %phi_ri [1x3] is the derivative of phi with respect to the 
+            %positional coordinates of bodyi. If I is the ground, then
+            %phi_rj is [0x0]
+            if dp1.bodyj.isground
+                phi_rj = [];
+            else
+                phi_rj = [0 0 0];
+            end
         end
+       
+        
+        function phi_pi = get.phi_pi(dp1) % s13 lecture 9.28.2016
+            %phi_pi [1x4] is the derivative of phi with respect to the 
+            %orientational coordinates of bodyi. If bodyi is the ground
+            %then phi_pi is [0x0]
+            if dp1.bodyi.isground
+                phi_pi = []
+            else
+                phi_pi = (dp1.bodyj.A*dp1.ajBar)'*MatOp.B(dp1.bodyi.p, dp1.aiBar);
+            end
+        end
+        
+        function phi_pj = get.phi_pj(dp1) % s13 lecture 9.28.2016
+            %phi_pi [1x4] is the derivative of phi with respect to the 
+            %orientational coordinates of bodyi. If bodyi is the ground
+            %then phi_pi is [0x0]
+            if dp1.bodyj.isground
+                phi_pj = [];
+            else
+                phi_pj =(dp1.bodyi.A*dp1.aiBar)'*MatOp.B(dp1.bodyj.p, dp1.ajBar);
+            end
             
-    end
+            end
+        end
+           
+            
     
 end
-
