5d3ce80b3d7096630d6e118abf8344e4ce376ea3
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-12-01 23:00:57 -0600

    added the B1 Constraint

---
+++ GCons/B1.m
@@ -0,0 +1,150 @@
+classdef B1
+    %B1 constraint using r-p formulation. The B1 is an intermediate level
+    %constraint which exists between a plane, formed by 2 vectors aiBar and
+    %biBar on body i, and a vector cjBar on body j.
+    %(note: see slide 22 from 9/26 for details and notation)
+    %the constraint is made using 2 dp1 constraints, and removes 2 degrees
+    %of freedom by enforcing 2 ACE's
+  
+    
+    properties
+        DOFremoved = 2;  %DOF removed
+        bodyi; %instance of a body object, i 
+        bodyj; %instance of a body object, j
+        dp1_ac;%instance of dp1 constraint between aiBar cjBar
+        dp1_bc;%instance of dp1 constraint between biBar cjBar 
+        
+        %these point definitions are taken from S22 lecture 9.26
+        aiHead; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        aiTail; %index of a [3x1] marker contained within bodyj, representing tail of aiBar
+        biHead; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        biTail; %index of a [3x1] marker contained within bodyj, representing tail of aiBar 
+        cjHead; %index of a [3x1] marker contained within bodyi, representing head of aiBar
+        cjTail; %index of a [3x1] marker contained within bodyj, representing tail of aiBar 
+  
+        %forcing fxns
+        f; %prescribed value that the dot product should assume
+        fdot; %derivative of above
+        fddot;%derivative of above 
+        t;  
+        
+    end
+    
+    %calculated values 
+    properties(Dependent)
+        aiBar; %vector in L-RF of body i        [3x1] 
+        biBar; %vector in L-RF of body i        [3x1] 
+        cjBar; %vector of L-RF of body j        [3x1]
+        
+        phi;   %constraint equation             [2x1] 
+        nu;    %-dPhi/ dt                       [2x1]
+        gamma; %RHS of acceleration equation    [2x1]
+        
+        phi_ri;%derivative of phi wrt ri        [2x3]  or [ ] if  i is ground
+        phi_rj;%derivative of phi wrt rj        [2x3]  or [ ] if  j is ground
+        phi_pi;%derivative of phi wrt ri        [2x4]  or [ ] if  i is ground
+        phi_pj;%derivative of phi wrt ri        [2x4]  or [ ] if  j is ground
+
+    end
+    
+    methods
+        %constructor
+        function b1 = B1(bodyi,bodyj,aiHead,aiTail,biHead,biTail,cjHead,cjTail,f,fdot,fddot,t)
+            
+            %bodies
+            b1.bodyi = bodyi;
+            b1.bodyj = bodyj;
+            
+            %vector head and tail indices
+            b1.aiHead = aiHead;
+            b1.aiTail = aiTail;
+            b1.biHead = biHead;
+            b1.biTail = biTail;
+            b1.cjHead = cjHead;
+            b1.cjTail = cjTail;
+            
+            %forcing fxn
+            b1.f = f; 
+            b1.fdot = fdot;
+            b1.fddot = fddot;
+            b1.t = t;
+            
+            %form basic Gcons which make up B1 Slide 
+            dp1_ac = DP1(b1.bodyi,b1.bodyj,b1.aiHead,b1.aiTail, b1.cjHead, b1.cjTail);
+            dp1_bc = DP1(b1.bodyi,b1.bodyj,b1.biHead,b1.biTail, b1.cjHead, b1.cjTail);
+            
+            
+        end
+        
+%--------------------getters for dependent properties-------------------------
+        
+         function aiBar = get.aiBar(b1) 
+            %ai vector in the L-RF
+            aiBar = b1.bodyi.markers{b1.aiHead}-b1.bodyi.markers{b1.aiTail};
+         end
+         
+         function biBar = get.biBar(b1) 
+            %bi vector in L-RF
+            biBar = b1.bodyi.markers{b1.biHead}-b1.bodyi.markers{b1.biTail};
+         end
+         
+         function cjBar = get.cjBar(b1)
+            %cj vector in L-RF
+            cjBar = b1.bodyj.markers{b1.cjHead}-b1.bodyi.markers{b1.cjTail};
+         end
+        
+         
+         
+        function phi = get.phi(b1)
+            %phi [2x1] constraint equation
+            phi = [b1.dp1_ac.phi , b1.dp1_bc.phi]';
+        end
+        
+        function nu = get.nu(b1) 
+           %nu [2x1] velocity equation [nu(1) , nu(2)]';
+           nu = [b1.dp1_ac.nu , b1.dp1_bc.nu]';
+        end
+        
+        function gamma = get.gamma(b1)
+            %gamma [2x1] is the RHS of the acceleration equation
+            gamma = [b1.dp1_ac.gamma , b1.dp1_bc.gamma]';
+        end
+       
+      
+        
+        function phi_ri = get.phi_ri(b1)
+            %phi_ri [2x3] is the derivative of both phi equations, with respect
+            %to the positional coordinates of body i, if body i is ground,
+            %phi_ri is []
+            phi_ri = [b1.dp1_ac.phi_ri, b1.dp1_bc.phi_ri]';
+          
+        end
+        
+         function phi_rj = get.phi_rj(b1)
+            %phi_rj [2x3] is the derivative of both phi equations, with respect
+            %to the positional coordinates of body i, if body i is ground,
+            %phi_rj is []
+            phi_rj = [b1.dp1_ac.phi_rj, b1.dp1_bc.phi_rj]';
+          
+         end
+        
+         function phi_pi = get.phi_pi(b1)
+            %phi_pi [2x3] is the derivative of both phi equations, with respect
+            %to the positional coordinates of body i, if body i is ground,
+            %phi_pi is []
+            phi_pi = [b1.dp1_ac.phi_pi, b1.dp1_bc.phi_pi]';
+          
+         end
+        
+          function phi_pj = get.phi_pj(b1)
+            %phi_pj [2x3] is the derivative of both phi equations, with respect
+            %to the positional coordinates of body i, if body i is ground,
+            %phi_pj is []
+            phi_pj = [b1.dp1_ac.phi_pj, b1.dp1_bc.phi_pj]';
+          
+          end
+        
+       
+
+    
+
