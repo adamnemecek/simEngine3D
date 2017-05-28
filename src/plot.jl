406493f07fa11dbf0b9e26a6bad47a4a2c3fe1e1
Alex Dawson-Elli <dawsonelli@wisc.edu>
2017-05-28 18:54:45 -0500

    restructured plotting routine

--- src/plot.jl
+++ src/plot.jl
@@ -1,39 +1,53 @@
 #imports
-using Gadfly
+  using Plots ; gr()
 
-function plot2DKinematics(bodyID::Array{Int},history::Array{SnapShot}; variable ="position" )
+
+function plot2DKinematics(bodyID,hist)
   """
   plots the position vs time for the bodies requested via their ID Number
   """
-  #get time
-  t = Array{Float64}
-  for snapshot in history
-    hcat(t,snapshot.q)
-  end
+    #get time , position, velocity, acceleration for specified body
+  t = hist.t
+  pos = hist.q[3*(bodyID-1)+1:3*bodyID ,:]
+  vel = hist.qdot[3*(bodyID-1)+1:3*bodyID ,:]
+  acc = hist.qddot[3*(bodyID-1)+1:3*bodyID ,:]
 
-  #which kinematic quantity should we look at
-  var = Array{Float64}
-  if variable == "pos"  || "position"
-    for snapshot in history
-      var = hcat(var,history.q)
-    end
-  elseif variable == "vel" || "velocity"
-    for snapshot in history
-      var = hcat(var,history.q)
-    end
-  elseif variable == "acc" || "acceleration"
-    for snapshot in history
-      var = hcat(var,history.q)
-    end
-  end
+  #reformat to x,y,z for plotting routine
+  x = [pos[1:1,:] ; vel[1:1,:] ; acc[1:1,:]]'
+  y = [pos[2:2,:] ; vel[2:2,:] ; acc[2:2,:]]'
+  z = [pos[3:3,:] ; vel[3:3,:] ; acc[3:3,:]]'
 
-#grab only the body we are interested in
- pltVar = var[3*(bodyID-1)+1:3*bodyID , :]
+#setup plot variables
+titles = ["body$(bodyID)position"  "velocity"  "acceleration"]
+labels = ["x" "x" "x" "y" "y" "y" "z" "z" "z"]
+ylabels = ["m" "m/s" "m/s²"]
+xlabels = ["t" "t" "t"]
 
-#plot the x,y,z position, acceleration or vel of specifed body
-plot(layer(x=t, y = pltVar[1,:], Geom.line),
-     layer(x=t, y = pltVar[2,:], Geom.line),
-     layer(x=t, y = pltVar[3,:], Geom.line),
-     Guide.xLabel("time"),
-     Guide.yLabel("position"));
+#execute plot
+plot(t,[x y z], layout = (3,1), title = titles, label = labels)
+plot!(ylabel = ylabels)
 end
+
+
+
+# """plots the 3D path of a body specified by bodyID"""
+# function plot3DKinematics(bodyID,history)
+#     # initialize the attractor
+#   n = 1500
+#   dt = 0.02
+#   σ, ρ, β = 10., 28., 8/3
+#   x, y, z = 1., 1., 1.
+#
+#   # initialize a 3D plot with 1 empty series
+#   plt = path3d(1, xlim=(-25,25), ylim=(-25,25), zlim=(0,50),
+#                   xlab = "x", ylab = "y", zlab = "z",
+#                   title = "Lorenz Attractor", marker = 1)
+#
+#   # build an animated gif, saving every 10th frame
+#   @gif for i=1:n
+#       dx = σ*(y - x)     ; x += dt * dx
+#       dy = x*(ρ - z) - y ; y += dt * dy
+#       dz = x*y - β*z     ; z += dt * dz
+#       push!(plt, x, y, z)
+#   end every 10
+# end
