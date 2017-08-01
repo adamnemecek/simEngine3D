902cd9c031308fb385d7b7034fb74977eaa6e8a3
Alex Dawson-Elli <dawsonelli@wisc.edu>
2017-08-01 15:29:36 -0500

    re-wrote body to using range indexing

--- src/body.jl
+++ src/body.jl
@@ -18,81 +18,62 @@ type Body
   end
 end
 
+#---------------------index accesser methods-----------------------------------
+"""returns a range object that specifies where the coordinates of a body are located in the system matrix"""
+rr(body::Body) = 3*(body.ID-1)+1:3*body.ID
+pr(body::Body) = 3*body.sim.nb+4(bd.ID-1)+1:3*body.sim.nb+4*body.ID
+
 #-----------------pseudo getter methods----------------------------------------
-function pt(bd::Body,ind::Int)
-  """retrieve a point from the point body point matrix pts"""
-  return bd.pts[:,ind:ind]
-end
+"""retrieve a point from the point body point matrix pts"""
+pt(bd::Body,ind::Int) =  bd.pts[:,ind:ind]
 
-function r(bd::Body)
-  """return r of body by accessing sim q"""
-  return bd.sim.q[3*(bd.ID-1)+1:3*bd.ID , 1:1]
-end
-function rdot(bd::Body)
-  """return rdot of body by accessing sim q"""
-    return bd.sim.qdot[3*(bd.ID-1)+1:3*bd.ID , 1:1]
-end
-function rddot(bd::Body)
-  """return rddot of body by accessing sim q"""
-  return bd.sim.qddot[3*(bd.ID-1)+1:3*bd.ID , 1:1]
-end
+"""get translations kinematics quantities"""
+r(bd::Body) = bd.sim.q[rr(bd) , 1:1]
+rdot(bd::Body) =  bd.sim.qdot[rr(bd) , 1:1]
+rddot(bd::Body) = bd.sim.qddot[rr(bd), 1:1]
 
-function p(bd::Body)
-  """return p of body by accessing sim q"""
-  return bd.sim.q[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1]
-end
-function pdot(bd::Body)
-  """return p of body by accessing sim q"""
-  return bd.sim.qdot[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1]
-end
-function pddot(bd::Body)
-  """return p of body by accessing sim q"""
-  return bd.sim.qddot[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1]
-end
+"""get orientation kinematics quantities"""
+p(bd::Body) =  bd.sim.q[pr(bd) , 1:1]
+pdot(bd::Body) = bd.sim.qdot[pr(bd), 1:1]
+pddot(bd::Body) = bd.sim.qddot[pr(bd) , 1:1]
 
-function A(bd::Body)
-  """returns rotation matrix of the body, calculated from p"""
-  return P2A(p(bd))
-end
-function G(bd::Body)
-  """returns G matrix of a body, calculated from p"""
-  return G(p(bd))
-end
-function Gdot(bd::Body)
-  """returns G matrix of a body, calculated from p"""
-  return G(pdot(bd))
-end
+"""returns rotation matrix of the body, calculated from p"""
+A(bd::Body) = P2A(p(bd))
+
+"""returns G matrix of a body, calculated from p"""
+G(bd::Body) =  G(p(bd))
+
+"""returns G matrix of a body, calculated from p"""
+Gdot(bd::Body) = G(pdot(bd))
 
 """take a 3x1 ωbar and a body and returns global pdot (for setting up IC's)"""
-function ωbar2pdot(bd::Body, ωbar::Array)
-  pdot = .5*G(bd)'*ωbar
-end
+ωbar2pdot(bd::Body, ωbar::Array) = .5*G(bd)'*ωbar
 
-function E(bd::Body)
-  """returns G matrix of a body, calculated from p"""
-  return E(p(bd))
-end
+"""returns G matrix of a body, calculated from p"""
+E(bd::Body) =  E(p(bd))
 
 
 #----------------------pseudo setter methods------------------------------------
+"""set translations kinematics quantities"""
 function set_r!(bd::Body, r::Array)
- bd.sim.q[3*(bd.ID-1)+1:3*bd.ID , 1:1] = r
+   bd.sim.q[rr(bd) , 1:1] = r
 end
 function set_rdot!(bd::Body, rdot::Array)
- bd.sim.qdot[3*(bd.ID-1)+1:3*bd.ID , 1:1] = rdot
+ bd.sim.qdot[rr(bd) , 1:1] = rdot
 end
 function set_rddot!(bd::Body , rddot::Array)
- bd.sim.qddot[3*(bd.ID-1)+1:3*bd.ID , 1:1] = rddot
+ bd.sim.qddot[rr(bd), 1:1] = rddot
 end
 
+"""set translations kinematics quantities"""
 function set_p!(bd::Body , p::Array)
- bd.sim.q[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1] = p
+ bd.sim.q[pr(bd) , 1:1] = p
 end
 function set_pdot!(bd::Body, pdot::Array)
- bd.sim.qdot[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1] = pdot
+ bd.sim.qdot[pr(bd) , 1:1] = pdot
 end
 function set_pddot!(bd::Body , pddot::Array)
- bd.sim.qddot[3*bd.sim.nb+4(bd.ID-1)+1:3*bd.sim.nb+4*bd.ID , 1:1] = pddot
+ bd.sim.qddot[pr(bd), 1:1] = pddot
 end
 
 """adds a new point to a bodies' point matrix"""
